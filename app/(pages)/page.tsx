fabc2b07eab87fe225700390aa045d9f76605360
Snack2132hub <jacksnack577@gmail.com>
2026-07-17 09:24:43 +0700

    ทดสอบแก้

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.wrapper}>
       <header className={styles.header}>
         <h1 className={styles.title}>My Project 
-          <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/phone-disconnected.png" alt="phone-disconnected"/>
+          
         </h1>
         
       </header>
