91c84ecf58d932b2f3a5215e0758f3d8da6b384a
Snack2132hub <jacksnack577@gmail.com>
2026-07-27 13:53:00 +0700

    แก้หน้าแรก

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "standalone",
+
   images: {
+    qualities: [75, 95], // เพิ่มบรรทัดนี้
+
     remotePatterns: [
       {
         protocol: "https",
